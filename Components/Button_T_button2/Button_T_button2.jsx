db12a06e40f9cd0b5145983e279971368c9c3097
Mohammed Farzaan <farzanmohd2286@gmail.com>
2024-11-20 12:05:38 +0530

    sfsdf

---
+++ Components/Button_T_button2/Button_T_button2.jsx
@@ -0,0 +1,16 @@
+
+const Button_T_button2 = () => {
+    return (
+        <div>
+            <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20">
+                <span className="text-lg">Shimmer</span>
+                <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
+                    <div className="relative h-full w-10 bg-white/20" />
+                </div>
+            </button>
+
+        </div>
+    )
+}
+
+export default Button_T_button2
